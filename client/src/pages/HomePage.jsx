70981d2322069dc51b0bd1cdb01e56741e4ec31c
Mujahid Hussain <hmujahid08@gmail.com>
2024-07-07 13:45:14 +0500

    admin panel

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { getProducts } from "../api/productApi";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const { userInfo } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -20,14 +22,22 @@ const HomePage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-gray-800 text-center font-bold my-4 text-3xl">
-        Featured Products
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((item) => (
-          <ProductCard key={item._id} product={item} />
-        ))}
-      </div>
+      {userInfo?.isAdmin !== true ? (
+        <>
+          <h1 className="text-gray-800 text-center font-bold my-4 text-3xl">
+            Featured Products
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.map((item) => (
+              <ProductCard key={item._id} product={item} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="text-center my-4">
+          <h1 className="text-gray-800 text-3xl font-bold">Admin Panel</h1>
+        </div>
+      )}
     </div>
   );
 };
