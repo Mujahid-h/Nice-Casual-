dd123ced81220e2c5d035f5b117b028e554c77ed
Mujahid Hussain <hmujahid08@gmail.com>
2024-07-13 23:27:45 +0500

    .

--- client/src/components/EditProduct.jsx
+++ client/src/components/EditProduct.jsx
@@ -89,7 +89,7 @@ const EditProduct = () => {
         </div>
         <div className="flex justify-end">
           <button
-            onClick={() => navigate("/a")}
+            onClick={() => navigate("/")}
             className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
           >
             Cancel
