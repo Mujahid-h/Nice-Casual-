a1522ab3b0c54574832d534d865a46bb5d2b5a34
Mujahid Hussain <hmujahid08@gmail.com>
2024-07-15 20:37:15 +0500

    hero

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -5,17 +5,21 @@ const HeroSection = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="anime w-[90%] mx-auto h-[80vh] mt-12 rounded-xl">
-      <div className="container mx-auto flex flex-col items-center justify-center h-full text-white">
+    <div
+      className="relative bg-no-repeat bg-cover h-[80vh] w-[80%] rounded-xl mx-auto mt-24"
+      style={{ backgroundImage: "url('./hero.png')" }}
+    >
+      <div className="absolute inset-0 bg-white opacity-20"></div>
+      <div className="container mx-auto flex flex-col items-center justify-center h-full text-black">
         <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center">
-          Welcome to Nice Casual
+          Welcome to Our Store
         </h1>
         <p className="text-xl md:text-2xl mb-8 text-center">
-          Shop with the latest trends
+          Discover the best products at unbeatable prices.
         </p>
         <button
           onClick={() => navigate("/shop")}
-          className="bg-transparent px-6 py-3 rounded text-lg border"
+          className="bg-blue-600 px-6 py-3 rounded text-lg hover:bg-blue-700 transition duration-300"
         >
           Shop Now
         </button>
