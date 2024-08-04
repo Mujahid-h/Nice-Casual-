34d3e719564659e67935411770f3d75f9ef3932a
Mujahid Hussain <hmujahid08@gmail.com>
2024-08-04 22:49:23 +0500

    texts

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -15,15 +15,17 @@ const ProductCard = ({ product }) => {
         />
       </div>
       <div className="px-3 py-2">
-        <h5 className="text-lg font-bold mb-1 truncate">{product.name}</h5>
-        <p className="text-xs text-gray-600 mb-2 line-clamp-2">
+        <h5 className="text-xl font-bold mb-1 truncate">{product.name}</h5>
+        <p className="text-sm text-gray-600 mb-2 line-clamp-2">
           {product.description}
         </p>
-        <p className="text-md font-semibold mb-2">${product.price}</p>
+        <p className="text-md font-bold mb-2 text-gray-700">
+          PKR. {product.price}
+        </p>
         <div className="flex justify-center mb-2">
           <Link
             to={`/product/${product._id}`}
-            className="bg-blue-500 text-white px-3 py-1.5  rounded text-sm my-2 hover:bg-blue-600 transition duration-300 w-full  text-center"
+            className="bg-blue-500 text-white px-3 py-2  rounded text-md my-2 hover:bg-blue-600 transition duration-300 w-full  text-center"
           >
             View Details
           </Link>
