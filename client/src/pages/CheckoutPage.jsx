dfe5458f8c8bf6a0b79689fc1a2ab46cb3f48d3e
Mujahid Hussain <hmujahid08@gmail.com>
2024-08-12 20:07:55 +0500

    "Updated warning message on CheckoutPage to add 'Thank You'"

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -226,6 +226,17 @@ const CheckoutPage = () => {
               <p>Price: PKR {item.price * item.quantity}</p>
             </div>
           ))}
+
+          {/* ========================================= */}
+
+          <div className="text-yellow-600 bg-yellow-100 p-4 rounded mb-4">
+            If you're experiencing issues with payment processing, please ensure
+            that any ad blockers or similar extensions are disabled for this
+            site.
+          </div>
+
+          {/* ========================================= */}
+
           <div className="font-bold text-xl mt-4 mb-4">
             Total: PKR {totalAmount}
           </div>
