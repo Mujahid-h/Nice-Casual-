fb5a2f8a8d9920534685fa7b1e9e19942214e8cf
Mujahid Hussain <hmujahid08@gmail.com>
2024-09-01 22:21:47 +0500

    "Added Redux Persist to store.js, refactored reducer configuration and middleware"

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
-import cartSlice from "./cartSlice";
-import orderSlice from "./orderSlice";
+import cartSLice from "./cartSlice";
+import orderSLice from "./orderSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  cart: cartSLice,
+  order: orderSLice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userSlice,
-    cart: cartSlice,
-    order: orderSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
